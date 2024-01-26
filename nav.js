5ed87b750286cdde465e4c115b1406688b57121e
jordanchen321 <140654452+jordanchen321@users.noreply.github.com>
2024-01-25 21:21:10 -0500

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,16 @@
+var manuBtn = document.getElementById("manuBtn")
+var sideNav = document.getElementById("sideNav")
+var menu = document.getElementById("menu")
+
+sideNav.style.right == "-250px";
+
+manuBtn.onclick = function(){
+    if(sideNav.style.right == "-250px"){
+        sideNav.style.right = "0";
+        menu.src = "images/close.png";
+    }
+    else{
+        sideNav.style.right = "-250px";
+        menu.src = "images/menu.png";
+    }
+}
